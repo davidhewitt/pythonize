aa961a9bbbcde6e4897aed973039c6a6169a2f5c
Juniper Tyree <50025784+juntyr@users.noreply.github.com>
2024-08-06 21:01:48 +0000

    Add test for unnamed and named mappings

--- tests/test_custom_types.rs
+++ tests/test_custom_types.rs
@@ -6,8 +6,8 @@ use pyo3::{
     types::{PyDict, PyMapping, PySequence, PyTuple},
 };
 use pythonize::{
-    depythonize, pythonize_custom, PythonizeListType, PythonizeMappingType, PythonizeTypes,
-    PythonizeUnnamedMappingAdapter, Pythonizer,
+    depythonize, pythonize_custom, PythonizeListType, PythonizeMappingType,
+    PythonizeNamedMappingType, PythonizeTypes, PythonizeUnnamedMappingAdapter, Pythonizer,
 };
 use serde::Serialize;
 use serde_json::{json, Value};
@@ -178,5 +178,104 @@ fn test_pythonizer_can_be_created() {
             .serialize(Pythonizer::custom::<PythonizeCustomDict>(py))
             .unwrap()
             .is_instance_of::<CustomDict>());
+    })
+}
+
+#[pyclass(mapping)]
+struct NamedCustomDict {
+    name: String,
+    items: HashMap<String, PyObject>,
+}
+
+#[pymethods]
+impl NamedCustomDict {
+    fn __len__(&self) -> usize {
+        self.items.len()
+    }
+
+    fn __getitem__(&self, key: String) -> PyResult<PyObject> {
+        self.items
+            .get(&key)
+            .cloned()
+            .ok_or_else(|| PyKeyError::new_err(key))
+    }
+
+    fn __setitem__(&mut self, key: String, value: PyObject) {
+        self.items.insert(key, value);
+    }
+
+    fn keys(&self) -> Vec<&String> {
+        self.items.keys().collect()
+    }
+
+    fn values(&self) -> Vec<PyObject> {
+        self.items.values().cloned().collect()
+    }
+}
+
+impl<'py> PythonizeNamedMappingType<'py> for NamedCustomDict {
+    type Builder = Bound<'py, NamedCustomDict>;
+
+    fn builder(py: Python<'py>, len: usize, name: &'static str) -> PyResult<Self::Builder> {
+        Bound::new(
+            py,
+            NamedCustomDict {
+                name: String::from(name),
+                items: HashMap::with_capacity(len),
+            },
+        )
+    }
+
+    fn push_field(
+        builder: &mut Self::Builder,
+        name: Bound<'py, pyo3::types::PyString>,
+        value: Bound<'py, PyAny>,
+    ) -> PyResult<()> {
+        unsafe { builder.downcast_unchecked::<PyMapping>() }.set_item(name, value)
+    }
+
+    fn finish(builder: Self::Builder) -> PyResult<Bound<'py, PyMapping>> {
+        Ok(unsafe { builder.into_any().downcast_into_unchecked() })
+    }
+}
+
+struct PythonizeNamedCustomDict;
+impl<'py> PythonizeTypes<'py> for PythonizeNamedCustomDict {
+    type Map = CustomDict;
+    type NamedMap = NamedCustomDict;
+    type List = PyTuple;
+}
+
+#[derive(Serialize)]
+struct Struct {
+    hello: u8,
+    world: i8,
+}
+
+#[test]
+fn test_custom_unnamed_dict() {
+    Python::with_gil(|py| {
+        PyMapping::register::<CustomDict>(py).unwrap();
+        let serialized =
+            pythonize_custom::<PythonizeCustomDict, _>(py, &Struct { hello: 1, world: 2 }).unwrap();
+        assert!(serialized.is_instance_of::<CustomDict>());
+
+        let deserialized: Value = depythonize(&serialized).unwrap();
+        assert_eq!(deserialized, json!({ "hello": 1, "world": 2 }));
+    })
+}
+
+#[test]
+fn test_custom_named_dict() {
+    Python::with_gil(|py| {
+        PyMapping::register::<NamedCustomDict>(py).unwrap();
+        let serialized =
+            pythonize_custom::<PythonizeNamedCustomDict, _>(py, &Struct { hello: 1, world: 2 })
+                .unwrap();
+        let named: Bound<NamedCustomDict> = serialized.extract().unwrap();
+        assert_eq!(named.borrow().name, "Struct");
+
+        let deserialized: Value = depythonize(&serialized).unwrap();
+        assert_eq!(deserialized, json!({ "hello": 1, "world": 2 }));
     })
 }
